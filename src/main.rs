3506539515cbd5f2f1516e664ef44d01418ee785
Soeking <great.from.g@gmail.com>
2020-05-08 23:04:30 +0900

    read file

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,16 @@
+use std::env;
+use std::fs::File;
+
 mod screen;
 mod key;
 
 fn main() {
-    screen::screen();
+    let args: Vec<String> = env::args().collect();
+    if args.len() > 1 {
+        let filename = args.get(1).unwrap();
+        let file = File::open(filename);
+        screen::screen(&mut file.unwrap());
+    } else {
+        println!("no file");
+    }
 }
--- src/screen.rs
+++ src/screen.rs
@@ -1,10 +1,14 @@
 use termion::screen::AlternateScreen;
-use std::io::{stdout, Write};
+use std::io::{stdout, Write, Read};
 use super::key;
+use std::fs::File;
 
-pub fn screen() {
+pub fn screen(file: &mut File) {
+    let mut buf = Vec::new();
+    let _ = file.read_to_end(&mut buf);
+
     let mut screen = AlternateScreen::from(stdout());
-    write!(screen, "alt now").unwrap();
+    write!(screen, "{:?}", buf).unwrap();
     screen.flush().unwrap();
 
     key::input();
