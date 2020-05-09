8b3726197e9ffc13c5e70f56f1de18f74cad91ef
Soeking <great.from.g@gmail.com>
2020-05-09 17:46:40 +0900

    bytes init

--- src/data.rs
+++ src/data.rs
@@ -1,4 +1,4 @@
-pub  struct Position {
+pub struct Position {
     row: u32,
     col: u8,
 }
@@ -11,9 +11,24 @@ pub struct Data {
 
 impl Data {
     pub fn new() -> Data {
-        let mut bin: Vec<u8> = Vec::new();
-        let mut ascii: Vec<char> = Vec::new();
+        let bin: Vec<u8> = Vec::new();
+        let ascii: Vec<char> = Vec::new();
         Data { bin, ascii, position: Position { row: 1, col: 1 } }
+    }
+
+    pub fn init(&mut self) {
+        let bytes = self.bin.clone();
+        for x in bytes {
+            match x {
+                32 => { self.ascii.push(' ') }
+                33...126 => { self.ascii.push(x as char) }
+                _ => { self.ascii.push('.') }
+            }
+        }
+    }
+
+    pub fn draw(&mut self) {
+        let max_row = &self.bin.len() / 16 + 1;
     }
 
     pub fn insert() {}
--- src/screen.rs
+++ src/screen.rs
@@ -8,10 +8,12 @@ use crate::data::Data;
 pub fn screen(file: &mut File) {
     let mut data = Data::new();
     let _ = file.read_to_end(&mut data.bin);
+    data.init();
 
     let mut screen = AlternateScreen::from(stdout());
     write!(screen, "{}", cursor::Goto(1, 1)).unwrap();
     write!(screen, "{:?}", data.bin).unwrap();
+    write!(screen, "{:?}", data.ascii).unwrap();
     screen.flush().unwrap();
 
     key::input();
