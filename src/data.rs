445c1c9a70c5c9252324544a50f9db4b2bcc70f6
Soeking <great.from.g@gmail.com>
2020-05-12 22:39:21 +0900

    move cursor

--- src/data.rs
+++ src/data.rs
@@ -2,21 +2,27 @@ use std::io::Write;
 use termion::*;
 
 pub struct Position {
-    row: u32,
-    col: u8,
+    pub row: u32,
+    pub col: u8,
+    pub col_id: u8,
+    pub char_pos: [u8; 48],
 }
 
 pub struct Data {
     pub bin: Vec<u8>,
     pub ascii: Vec<char>,
     pub position: Position,
+    pub max_row: usize,
 }
 
 impl Data {
     pub fn new() -> Data {
         let bin: Vec<u8> = Vec::new();
         let ascii: Vec<char> = Vec::new();
-        Data { bin, ascii, position: Position { row: 1, col: 11 } }
+        let pos: [u8; 48] = [11, 12, 14, 15, 17, 18, 20, 21, 23, 24, 26, 27, 29, 30, 32, 33,
+            35, 36, 38, 39, 41, 42, 44, 45, 47, 48, 50, 51, 53, 54, 56, 57,
+            62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77];
+        Data { bin, ascii, position: Position { row: 1, col: 11, col_id: 0, char_pos: pos }, max_row: 1 }
     }
 
     pub fn init(&mut self) {
@@ -28,17 +34,17 @@ impl Data {
                 _ => { self.ascii.push('.') }
             }
         }
+        self.max_row = self.bin.len() / 16 + 1;
     }
 
     pub fn draw<T: Write>(&mut self, screen: &mut T) {
-        let max_row = &self.bin.len() / 16 + 1;
         let mut address = 0;
-        for i in 0..max_row {
+        for i in 0..self.max_row {
             write!(screen, "{}", cursor::Goto(1, (i + 1) as u16)).unwrap();
             write!(screen, "{:>08x}  ", address);
 
             let range =
-                if i != max_row - 1 { i * 16..(i + 1) * 16 } else { i * 16..self.bin.len() };
+                if i != self.max_row - 1 { i * 16..(i + 1) * 16 } else { i * 16..self.bin.len() };
 
             for x in range.clone() {
                 write!(screen, "{:>02x} ", &self.bin[x]);
@@ -46,12 +52,38 @@ impl Data {
 
             write!(screen, "{}|", cursor::Goto(61, (i + 1) as u16));
             for x in range.clone() {
-                write!(screen, "{} ", &self.ascii[x]);
+                write!(screen, "{}", &self.ascii[x]);
             }
             write!(screen, "|");
             address += 16;
         }
         write!(screen, "{}{}{}", cursor::Goto(11, 1), cursor::BlinkingBlock, cursor::Show);
+    }
+
+    pub fn move_down(&mut self) {
+        if self.position.row < self.max_row as u32 {
+            self.position.row += 1;
+        }
+    }
+
+    pub fn move_up(&mut self) {
+        if self.position.row > 0 {
+            self.position.row -= 1;
+        }
+    }
+
+    pub fn move_right(&mut self) {
+        if self.position.col_id < 47 {
+            self.position.col_id += 1;
+            self.position.col = self.position.char_pos[self.position.col_id as usize];
+        }
+    }
+
+    pub fn move_left(&mut self) {
+        if self.position.col_id > 0 {
+            self.position.col_id -= 1;
+            self.position.col = self.position.char_pos[self.position.col_id as usize];
+        }
     }
 
     pub fn insert() {}
--- src/key.rs
+++ src/key.rs
@@ -2,19 +2,29 @@ use std::io::{stdin, Write};
 use termion::input::TermRead;
 use termion::event::{Key, Event};
 use crate::data::Data;
+use termion::cursor;
 
-pub fn input(data: &mut Data, screen: &mut dyn Write) {
+pub fn input<T: Write>(data: &mut Data, screen: &mut T) {
     let stdin = stdin();
 
     for event in stdin.events() {
         match event.unwrap() {
             Event::Key(Key::Ctrl('q')) => { break; }
-            Event::Key(Key::Down) => {}
-            Event::Key(Key::Up) => {}
-            Event::Key(Key::Right) => {}
-            Event::Key(Key::Left) => {}
+            Event::Key(Key::Down) => { data.move_down(); }
+            Event::Key(Key::Up) => { data.move_up(); }
+            Event::Key(Key::Right) => { data.move_right(); }
+            Event::Key(Key::Left) => { data.move_left(); }
             Event::Key(Key::Char(c)) => {}
             _ => {}
         }
+
+        write!(
+            screen,
+            "{}{}{}",
+            cursor::Goto(data.position.col as u16, data.position.row as u16),
+            cursor::BlinkingBlock,
+            cursor::Show
+        );
+        screen.flush().unwrap();
     }
 }
